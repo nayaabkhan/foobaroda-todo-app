983d3043c09ffa55b1a1b2cacc0197fd4fe374ae
Nayaabkhan Khan <khannayaab@gmail.com>
2015-05-06 08:48:15 +0530

    Adding new todos

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ var app = angular.module('foodoApp', []);
 
 app.controller('mainController', function ($scope) {
 
+  // model to hold the new todo text
+  $scope.newTodoText = '';
+
   // mock todos
   $scope.todoList = [
     { text: 'Shave', done: false },
     { text: 'Clean the house', done: false },
     { text: 'Share selfies on FB', done: true }
   ];
+
+
+  // method to add a new todo to the todoList
+  $scope.addNewTodo = function () {
+    if ($scope.newTodoText.trim() === '') {
+      return;
+    }
+
+    $scope.todoList.push({
+      text: $scope.newTodoText,
+      done: false
+    });
+    $scope.newTodoText = '';
+  };
 
 });
