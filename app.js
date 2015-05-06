b88f57745bcc8ad5c8a4257bcf5a3ffb002105dd
Nayaabkhan Khan <khannayaab@gmail.com>
2015-05-06 08:21:37 +0530

    Adding and listing mock todos

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // create fooDoApp module
 var app = angular.module('foodoApp', []);
+
+app.controller('mainController', function ($scope) {
+
+  // mock todos
+  $scope.todoList = [
+    { text: 'Shave', done: false },
+    { text: 'Clean the house', done: false },
+    { text: 'Share selfies on FB', done: true }
+  ];
+
+});
